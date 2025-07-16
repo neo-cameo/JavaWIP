cc271c3ad1267f5de3a31f3099ae0ee30ecf5c70
Maceo <ssjmaceo@gmail.com>
2025-07-16 16:42:33 -0400

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,84 @@
-console.log('Hello World' [10])
-console.log('Hello World'.length)
-/**
- * 
- */
+// console.log('Hello World')
+// console.log('Hello World'.length)
+// /**
+//  * 
+//  */
+// let fullName = 'Maceo'
+// let celsius = 10
+// let fahrenheit = celsius * 1.8 + 32
+// console.log(fahrenheit)
+
+
+// let subscribed = false
+// let loggedIn = true
+
+// if (subscribed === true) {
+//     console.log('show the video')
+// }
+// else if (loggedIn === true) {
+//     console.log('tell user to upgrade their account')
+// }
+// else {
+//     console.log('tell user to log into their account')
+// }
+
+
+// let cash = 50
+// let price = 40
+// let OpenStore = true
+
+// if (cash >= price && OpenStore) {
+//     console.log(`Here's your receipt and ${cash-price} dollars change`)
+// }
+// else if (cash === price && OpenStore) {
+//     console.log("You paid the exact amount")
+// }
+// else {
+//     console.log(`Don't give receipt`)
+// }
+
+
+// let money = 50
+// let cost = 40
+// let isStoreOpen = true
+
+// if (money >= cost && isStoreOpen === true) {
+//     console.log('print the receipt')
+// }
+
+
+// let subbed = false
+// let login = true
+
+// let str = subbed || login ? 'show the video' : 'hide the video'
+// console.log(str)
+
+
+// let count = 1
+
+// while (count <= 100) {
+//     console.log(count)
+//     count = count + 1
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 ===0 && i % 5 === 0) {
+//         console.log (`${i} -> Frontend Simplified`)
+//     } 
+//     else if (i % 3 === 0) {
+//         console.log(`${i} -> Frontend`)
+//     }
+//     else if (i % 5 ===0) {
+//         console.log (`${i} -> Simplified`)
+//     }
+//     else {
+//         console.log(`${i} -> ${i}`)
+//     }
+// }
+
+
+const str = "Frontend Simplified"
+
+for (let i=0; i<str.length; ++i) {
+    console.log(str [i])
+}
